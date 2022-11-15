53d4cf028768231b53ad3a3381ff4679460d5978
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2022-11-15 22:32:38 +0100

    Tutorials from youtube

---
+++ hello.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+# This is a comment in shell scripting
+echo "Hello World"
+
+#System variables
+echo Our shell name is $BASH
+echo Our shell version is $BASH_VERSION
+echo This is the home directory- $HOME
+echo Our current working directory is $PWD
+
+#User defined variables
+name=Muyiwa
+valueTen=10
+echo My name is $name
+echo value $valueTen
