c6d7386f355f1b6b32e42dfa96df4276df2c180e
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2022-11-23 14:54:04 +0100

    Arithmetic Operations

---
+++ arith10.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+num1=20
+num2=7
+# To deal wih arithmetic it should be in double braces (())
+
+echo $(( num1 + num2 ))
+echo $(( num1 - num2 ))
+echo $(( num1 * num2 ))
+echo $(( num1 / num2 ))
+echo $(( num1 % num2 ))
--- logicalORoperator9.sh
+++ logicalORoperator9.sh
@@ -10,3 +10,8 @@ else
     echo "Your age: $age is not accepted"
 fi
 
+# if [ "$age" -gt 18 -o "$age" -lt 30 ]
+# 2nd way to use OR operator is by using the -a flag
+
+# if [[ "$age" -gt 18 || "$age" -lt 30 ]]
+# 3rd way to writing and AND operator
