5836f6c3daa0eabd2396c359d15c79ad8163c887
Oluwamuyiwa Dosunmu <oluwamuyiwadosunmu@gmail.com>
2022-11-23 15:03:52 +0100

    Arithmetic Operations

--- arith10.sh
+++ arith10.sh
@@ -9,3 +9,13 @@ echo $(( num1 - num2 ))
 echo $(( num1 * num2 ))
 echo $(( num1 / num2 ))
 echo $(( num1 % num2 ))
+
+# Another way is by using expr keyword which allows for single parentheses
+# for this method, the multiplication must have an escape character
+
+echo $(expr $num1 + $num2 )
+echo $(expr $num1 - $num2 )
+echo $(expr $num1 \* $num2 )
+echo $(expr $num1 / $num2 )
+echo $(expr $num1 % $num2 )
+
